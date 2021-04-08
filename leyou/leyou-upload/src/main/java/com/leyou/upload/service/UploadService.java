a1ed1d8ce5511e0addad0c58d359be3b6d1339f4
jiangbiao <1367033096@qq.com>
2021-04-09 01:36:37 +0800

    文件上传已改造，通过fastDFS上传到远程Linux服务器了

--- leyou/leyou-upload/src/main/java/com/leyou/upload/service/UploadService.java
+++ leyou/leyou-upload/src/main/java/com/leyou/upload/service/UploadService.java
@@ -1,7 +1,11 @@
 package com.leyou.upload.service;
 
+import com.github.tobato.fastdfs.domain.StorePath;
+import com.github.tobato.fastdfs.service.FastFileStorageClient;
+import org.apache.commons.lang.StringUtils;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.web.multipart.MultipartFile;
 
@@ -18,9 +22,10 @@ public class UploadService {
     private static final List<String> CONTENT_TYPES = Arrays.asList("image/jpeg", "image/gif");
     private static final Logger LOGGER = LoggerFactory.getLogger(UploadService.class);
 
+    @Autowired
+    private FastFileStorageClient storageClient;
+
     public String uploadImage(MultipartFile file) {
-
-
 
 
             String originalFilename = file.getOriginalFilename();
@@ -41,10 +46,15 @@ public class UploadService {
                 }
 
                 // 保存到服务器
-                file.transferTo(new File("E:\\IdeaProjects\\image\\" + originalFilename));
+                //file.transferTo(new File("E:\\IdeaProjects\\image\\" + originalFilename)); //最开始保存到本地
+                String ext = StringUtils.substringAfterLast(originalFilename, ".");
+                StorePath storePath = this.storageClient.uploadFile(file.getInputStream(), file.getSize(), ext, null);
 
                 // 生成url地址，返回
-                return "http://image.leyou.com/" + originalFilename;
+                //return "http://image.leyou.com/" + originalFilename; //文件上传到Linux远程，这里也要改
+                return "http://image.leyou.com/"+storePath.getFullPath();
+
+
             } catch (IOException e) {
                 LOGGER.info("服务器内部错误：{}", originalFilename);
                 e.printStackTrace();

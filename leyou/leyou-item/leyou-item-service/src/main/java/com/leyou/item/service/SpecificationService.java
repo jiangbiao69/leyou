c37cbce85c3d09a5a10b0679e91abe6c6ddc67e3
jiangbiao <1367033096@qq.com>
2021-04-16 18:11:13 +0800

    规格参数查询已做完

---
+++ leyou/leyou-item/leyou-item-service/src/main/java/com/leyou/item/service/SpecificationService.java
@@ -0,0 +1,36 @@
+package com.leyou.item.service;
+
+import com.leyou.item.mapper.SpecGroupMapper;
+import com.leyou.item.mapper.SpecParamMapper;
+import com.leyou.item.pojo.SpecGroup;
+import com.leyou.item.pojo.SpecParam;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@Service
+public class SpecificationService {
+    @Autowired
+    private SpecGroupMapper groupMapper;
+    @Autowired
+    private SpecParamMapper paramMapper;
+
+    /*
+    根据分类ID查询参数组
+    */
+    public List<SpecGroup> queryGroupByCid(Long cid) {
+        SpecGroup record = new SpecGroup();
+        record.setCid(cid);
+        return this.groupMapper.select(record);
+    }
+
+    /*
+    根据查询条件查询规格参数
+    */
+    public List<SpecParam> queryParams(Long gid) {
+        SpecParam record = new SpecParam();
+        record.setGroupId(gid);
+        return this.paramMapper.select(record);
+    }
+}

662239dc4278ddb25f4c174692f9c23060e7b02e
jiangbiao <1367033096@qq.com>
2021-05-03 22:27:15 +0800

    第16天的用户注册已完成，虽然阿里云短信无法发送，但是我们可以手动在Redis中添加校验码，同样可以完成注册

--- leyou/leyou-user/leyou-user-service/src/main/java/com/leyou/user/controller/UserController.java
+++ leyou/leyou-user/leyou-user-service/src/main/java/com/leyou/user/controller/UserController.java
@@ -1,5 +1,6 @@
 package com.leyou.user.controller;
 
+import com.leyou.user.pojo.User;
 import com.leyou.user.service.UserService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
@@ -9,6 +10,8 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.validation.Valid;
 
 
 @Controller
@@ -30,5 +33,20 @@ public class UserController {
     public ResponseEntity<Void> sendVerifyCode(@RequestParam("phone")String phone){
         this.userService.sendVerifyCode(phone);
         return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @PostMapping("register")
+    public ResponseEntity<Void> register(@Valid User user, @RequestParam("code")String code){
+        this.userService.register(user,code);
+        return ResponseEntity.status(HttpStatus.CREATED).build();
+    }
+
+    @GetMapping("query")
+    public ResponseEntity<User> queryUser(@RequestParam("username")String username,@RequestParam("password")String password){
+        User user=this.userService.queryUser(username,password);
+        if (user==null){
+            return ResponseEntity.badRequest().build();
+        }
+        return ResponseEntity.ok(user);
     }
 }
